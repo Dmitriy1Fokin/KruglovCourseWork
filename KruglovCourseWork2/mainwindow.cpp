c00904c1da7f9309deb67ca2d5f1d0ae49d3dccf
DmitriyFokin <fokinds1@gmail.com>
2018-03-30 14:13:12 +0300

    Started

---
+++ KruglovCourseWork2/mainwindow.cpp
@@ -0,0 +1,153 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "console.h"
+#include "settingsdialog.h"
+
+#include <QMessageBox>
+#include <QLabel>
+#include <QtSerialPort/QSerialPort>
+
+//! [0]
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+//! [0]
+    ui->setupUi(this);
+    console = new Console;
+    console->setEnabled(false);
+    setCentralWidget(console);
+//! [1]
+    serial = new QSerialPort(this);
+//! [1]
+    settings = new SettingsDialog;
+
+    ui->actionConnect->setEnabled(true);
+    ui->actionDisconnect->setEnabled(false);
+    ui->actionQuit->setEnabled(true);
+    ui->actionConfigure->setEnabled(true);
+
+    status = new QLabel;
+    ui->statusBar->addWidget(status);
+
+    initActionsConnections();
+
+    connect(serial, static_cast<void (QSerialPort::*)(QSerialPort::SerialPortError)>(&QSerialPort::error),
+            this, &MainWindow::handleError);
+
+//! [2]
+    connect(serial, &QSerialPort::readyRead, this, &MainWindow::readData);
+//! [2]
+    connect(console, &Console::getData, this, &MainWindow::writeData);
+//! [3]
+}
+//! [3]
+
+MainWindow::~MainWindow()
+{
+    delete settings;
+    delete ui;
+}
+
+//! [4]
+void MainWindow::openSerialPort()
+{
+    SettingsDialog::Settings p = settings->settings();
+    serial->setPortName(p.name);
+    serial->setBaudRate(p.baudRate);
+    serial->setDataBits(p.dataBits);
+    serial->setParity(p.parity);
+    serial->setStopBits(p.stopBits);
+    serial->setFlowControl(p.flowControl);
+    if (serial->open(QIODevice::ReadWrite)) {
+        console->setEnabled(true);
+        console->setLocalEchoEnabled(p.localEchoEnabled);
+        ui->actionConnect->setEnabled(false);
+        ui->actionDisconnect->setEnabled(true);
+        ui->actionConfigure->setEnabled(false);
+        showStatusMessage(tr("Connected to %1 : %2, %3, %4, %5, %6")
+                          .arg(p.name).arg(p.stringBaudRate).arg(p.stringDataBits)
+                          .arg(p.stringParity).arg(p.stringStopBits).arg(p.stringFlowControl));
+    } else {
+        QMessageBox::critical(this, tr("Error"), serial->errorString());
+
+        showStatusMessage(tr("Open error"));
+    }
+}
+//! [4]
+
+//! [5]
+void MainWindow::closeSerialPort()
+{
+    if (serial->isOpen())
+        serial->close();
+    console->setEnabled(false);
+    ui->actionConnect->setEnabled(true);
+    ui->actionDisconnect->setEnabled(false);
+    ui->actionConfigure->setEnabled(true);
+    showStatusMessage(tr("Disconnected"));
+}
+//! [5]
+
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("About Simple Terminal"),
+                       tr("The <b>Simple Terminal</b> example demonstrates how to "
+                          "use the Qt Serial Port module in modern GUI applications "
+                          "using Qt, with a menu bar, toolbars, and a status bar."));
+}
+
+//! [6]
+void MainWindow::writeData(const QByteArray &data)
+{
+    serial->write(data);
+}
+//! [6]
+
+//! [7]
+void MainWindow::readData()
+{
+    QByteArray data = serial->readAll();
+    console->putData(data);
+
+
+    /////////////////////////////////////////////////////////
+    str.push_back(QString(data));
+    if (str[11] == '8')
+    {
+        QMessageBox* pmbx = new QMessageBox(QMessageBox::Information,
+                                            "Право доступа",
+                                            str);
+        pmbx->exec();
+        delete pmbx;
+        str.clear();
+    }
+
+}
+//! [7]
+
+//! [8]
+void MainWindow::handleError(QSerialPort::SerialPortError error)
+{
+    if (error == QSerialPort::ResourceError) {
+        QMessageBox::critical(this, tr("Critical Error"), serial->errorString());
+        closeSerialPort();
+    }
+}
+//! [8]
+
+void MainWindow::initActionsConnections()
+{
+    connect(ui->actionConnect, &QAction::triggered, this, &MainWindow::openSerialPort);
+    connect(ui->actionDisconnect, &QAction::triggered, this, &MainWindow::closeSerialPort);
+    connect(ui->actionQuit, &QAction::triggered, this, &MainWindow::close);
+    connect(ui->actionConfigure, &QAction::triggered, settings, &MainWindow::show);
+    connect(ui->actionClear, &QAction::triggered, console, &Console::clear);
+    connect(ui->actionAbout, &QAction::triggered, this, &MainWindow::about);
+    connect(ui->actionAboutQt, &QAction::triggered, qApp, &QApplication::aboutQt);
+}
+
+void MainWindow::showStatusMessage(const QString &message)
+{
+    status->setText(message);
+}
